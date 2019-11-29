9a1ed5b8d33dd0504f1122c617e2351277529de1
Dinh Ton <dinh19197@gmail.com>
2019-11-29 16:36:44 +0700

    - Add Disjoin Set Union.

---
+++ src/Graph/DSU.java
@@ -0,0 +1,9 @@
+package Graph;
+
+/**
+ * Description: Implemented Data Structure DisjoinSetUnion.
+ * Time Complexity: O(alpha * N).
+ */
+public class DSU {
+
+}
--- src/Math/Gauss.java
+++ src/Math/Gauss.java
@@ -4,7 +4,7 @@ package Math;
  * This class implemented gaussian Elimination Process
  * Input: a matrix with size m x n
  * Output: a matrix with size m x n after apply Gaussian Elimination Process
- * Time Complexity: O(n^3)
+ * Time Complexity: O(n^3).
  */
 
 public class Gauss {
